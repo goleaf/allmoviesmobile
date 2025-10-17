4d55c025cef289c0a02b48a910ffbd1878f8ebc0
Andrej Prus <goleaf@gmail.com>
2025-10-17 15:31:19 +0200

    Add extended external link support

--- lib/data/models/external_ids_model.g.dart
+++ lib/data/models/external_ids_model.g.dart
@@ -7,7 +7,19 @@ part of 'external_ids_model.dart';
 // **************************************************************************
 
 _$ExternalIdsImpl _$$ExternalIdsImplFromJson(Map<String, dynamic> json) =>
-    _$ExternalIdsImpl(imdbId: json['imdb_id'] as String?);
+    _$ExternalIdsImpl(
+      imdbId: json['imdb_id'] as String?,
+      facebookId: json['facebook_id'] as String?,
+      twitterId: json['twitter_id'] as String?,
+      tvdbId: json['tvdb_id'] as String?,
+      tvrageId: json['tvrage_id'] as String?,
+    );
 
 Map<String, dynamic> _$$ExternalIdsImplToJson(_$ExternalIdsImpl instance) =>
-    <String, dynamic>{'imdb_id': instance.imdbId};
+    <String, dynamic>{
+      'imdb_id': instance.imdbId,
+      'facebook_id': instance.facebookId,
+      'twitter_id': instance.twitterId,
+      'tvdb_id': instance.tvdbId,
+      'tvrage_id': instance.tvrageId,
+    };

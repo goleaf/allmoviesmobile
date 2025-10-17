beeae7c8d783140de6be826925c651c1388c1a6e
Andrej Prus <goleaf@gmail.com>
2025-10-17 15:29:20 +0200

    Add rich browse experience for TV shows

--- lib/providers/series_provider.dart
+++ lib/providers/series_provider.dart
@@ -3,13 +3,32 @@ import '../data/models/paginated_response.dart';
 import '../data/tmdb_repository.dart';
 import 'paginated_resource_provider.dart';
 
+enum SeriesRequestType { category, discover }
+
 class SeriesProvider extends PaginatedResourceProvider<Movie> {
-  SeriesProvider(this._repository, {this.category = 'popular'}) {
+  SeriesProvider(
+    this._repository, {
+    String? category,
+    Map<String, String>? discoverFilters,
+    SeriesRequestType requestType = SeriesRequestType.category,
+  })  : category = category ?? 'popular',
+        _requestType = requestType,
+        _discoverFilters = Map.unmodifiable(discoverFilters ?? const {}),
+        assert(
+          requestType == SeriesRequestType.category ||
+              (discoverFilters != null && discoverFilters.isNotEmpty),
+          'Discover filters are required when using SeriesRequestType.discover',
+        ) {
     loadInitial();
   }
 
   final TmdbRepository _repository;
+  final SeriesRequestType _requestType;
+  final Map<String, String> _discoverFilters;
   final String category;
+
+  SeriesRequestType get requestType => _requestType;
+  Map<String, String> get discoverFilters => _discoverFilters;
 
   List<Movie> get series => items;
   bool get isLoading => isInitialLoading;
@@ -21,10 +40,19 @@ class SeriesProvider extends PaginatedResourceProvider<Movie> {
 
   @override
   Future<PaginatedResponse<Movie>> loadPage(int page, {bool forceRefresh = false}) {
-    return _repository.fetchTvCategory(
-      category: category,
-      page: page,
-      forceRefresh: forceRefresh,
-    );
+    switch (_requestType) {
+      case SeriesRequestType.category:
+        return _repository.fetchTvCategory(
+          category: category,
+          page: page,
+          forceRefresh: forceRefresh,
+        );
+      case SeriesRequestType.discover:
+        return _repository.discoverTvSeries(
+          filters: _discoverFilters,
+          page: page,
+          forceRefresh: forceRefresh,
+        );
+    }
   }
 }
